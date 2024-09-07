92de7757c2286b28628201a926d9ea88856caf29
lvyufeng <lvyufeng@cqu.edu.cn>
2024-09-07 21:45:16 +0800

    add resume download method

--- download_whl.py
+++ download_whl.py
@@ -7,6 +7,7 @@ from datetime import datetime
 candidate_list = ['unified/x86_64/', 'cpu/x86_64/', 'cpu/aarch64/']
 
 def gen_url(path):
+    max_try_time = 5
     current_year_month = datetime.now().strftime("%Y%m")
     current_year_month_day = datetime.now().strftime("%Y%m%d")
     url_prefix = f'http://repo.mindspore.cn/mindspore/mindspore/version/{current_year_month}/{current_year_month_day}'
@@ -29,7 +30,8 @@ def gen_url(path):
         pattern = re.compile(r'<a href="mindspore-(.*?).whl"')
         matches = re.findall(pattern, html)
 
-        for match in matches:
+        while matches:
+            match = matches.pop(0)
             whl_name = 'mindspore-' + match + '.whl'
             idx = match.index('cp')
             save_name = 'mindspore-newest-' + match[idx:] + '.whl'
@@ -40,13 +42,17 @@ def gen_url(path):
 
             # 打开本地文件，以二进制写入模式
             print('Start download：', whl_name)
-            with open(path + save_name, 'wb') as f:
-                # 循环读取响应内容的每个数据块，然后写入本地文件
-                for chunk in response.iter_content(chunk_size=8192):
-                    if chunk:
-                        f.write(chunk)
+            try:
+                with open(path + save_name, 'wb') as f:
+                    # 循环读取响应内容的每个数据块，然后写入本地文件
+                    for chunk in response.iter_content(chunk_size=8192):
+                        if chunk:
+                            f.write(chunk)
+                print('Download：', save_name)
+            except:
+                print('Resume', match)
+                matches.append(match)
 
-            print('Download：', save_name)
 
 
 if __name__ == '__main__':
